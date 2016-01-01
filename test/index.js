7dcd65eb0e65472907c7c29508865dca4f13f76e
1000ch <shogo.sensui@gmail.com>
2016-01-02 07:15:37 +0900

    use async/await syntax

--- test/index.js
+++ test/index.js
@@ -1,28 +1,32 @@
 import test from 'ava';
 import rog from '../';
 
-test('resolve with fixed format json', t => {
-  return rog('http://google.com').then(data => {
-    t.plan(7);
-    t.not(data.title, undefined, 'title is not undefined');
-    t.not(data.type, undefined, 'type is not undefined');
-    t.not(data.url, undefined, 'url is not undefined');
-    t.not(data.image, undefined, 'image is not undefined');
-    t.not(data.site, undefined, 'site is not undefined');
-    t.not(data.description, undefined, 'description is not undefined');
-    t.not(data.locale, undefined, 'locale is not undefined');
-  });
+test('resolve with fixed format json', async function (t) {
+  let data = await rog('http://google.com');
+  t.plan(7);
+  t.not(data.title, undefined, 'title is not undefined');
+  t.not(data.type, undefined, 'type is not undefined');
+  t.not(data.url, undefined, 'url is not undefined');
+  t.not(data.image, undefined, 'image is not undefined');
+  t.not(data.site, undefined, 'site is not undefined');
+  t.not(data.description, undefined, 'description is not undefined');
+  t.not(data.locale, undefined, 'locale is not undefined');
 });
 
-test('reject if argument is none', t => {
-  return rog().catch(error => {
+test('reject if argument is none', async function (t) {
+  try {
+    await rog();
+  } catch (error) {
+    t.plan(1);
     t.pass(error);
-  });
+  }
 });
 
-test('reject if response is not HTML', t => {
-  let image = 'http://placehold.it/1x1';
-  return rog(image).catch(error => {
+test('reject if response is not HTML', async function (t) {
+  try {
+    await rog('http://placehold.it/1x1');
+  } catch (error) {
+    t.plan(1);
     t.pass(error);
-  });
+  }
 });
