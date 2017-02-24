de08563edc24d1cb374a6f63c2383172482ae309
1000ch <shogo.sensui@gmail.com>
2017-02-24 23:22:37 +0900

    Add iso-8859-1 test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import rog from './';
 
-test('resolve with fixed format json', async t => {
+test('parse utf-8 encoded HTML', async t => {
   let data = await rog('https://google.com', null, {
     title: require('./packages/rog-plugin-title'),
     type: require('./packages/rog-plugin-type'),
@@ -26,6 +26,29 @@ test('resolve with fixed format json', async t => {
 
 test('parse EUC-JP encoded HTML', async t => {
   let data = await rog('https://mixi.jp/', null, {
+    title: require('./packages/rog-plugin-title'),
+    type: require('./packages/rog-plugin-type'),
+    url: require('./packages/rog-plugin-url'),
+    image: require('./packages/rog-plugin-image'),
+    images: require('./packages/rog-plugin-images'),
+    sitename: require('./packages/rog-plugin-sitename'),
+    description: require('./packages/rog-plugin-description'),
+    locale: require('./packages/rog-plugin-locale')
+  });
+
+  t.plan(8);
+  t.not(data.title, undefined, 'title is not undefined');
+  t.not(data.type, undefined, 'type is not undefined');
+  t.not(data.url, undefined, 'url is not undefined');
+  t.not(data.image, undefined, 'image is not undefined');
+  t.true(Array.isArray(data.images), 'images is empty');
+  t.not(data.sitename, undefined, 'sitename is not undefined');
+  t.not(data.description, undefined, 'description is not undefined');
+  t.not(data.locale, undefined, 'locale is not undefined');
+});
+
+test('parse iso-8859-1 encoded HTML', async t => {
+  let data = await rog('http://www.mhlw.go.jp/index.shtml', null, {
     title: require('./packages/rog-plugin-title'),
     type: require('./packages/rog-plugin-type'),
     url: require('./packages/rog-plugin-url'),
