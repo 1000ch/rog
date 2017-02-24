68a755b24c693edbe1165efcb1540ddc316c3bb1
1000ch <shogo.sensui@gmail.com>
2017-02-24 15:40:17 +0900

    Use charset in response header
    Replace eslint with xo

--- index.js
+++ index.js
@@ -7,6 +7,34 @@ const cheerio = require('cheerio');
 const jschardet = require('jschardet');
 const iconv = require('iconv-lite');
 
+const getBody = response => {
+  const header = response.header || {};
+  const body = response.body;
+
+  const contentType = header['content-type'] || '';
+  const matches = contentType.match(/charset=(.+)/);
+  if (matches !== null) {
+    return body.toString(matches[1]);
+  }
+
+  const result = jschardet.detect(body);
+  if (result && result.encoding && (result.confidence || 0) >= 0.99) {
+    return iconv.decode(body, result.encoding);
+  }
+
+  const head = body.toString('ascii').match(/<head[\s>]([\s\S]*?)<\/head>/i);
+  if (!head) {
+    return body.toString('utf8');
+  }
+
+  const charset = head[1].match(/<meta[^>]*[\s;]+charset\s*=\s*["']?([\w\-_]+)["']?/i);
+  if (charset) {
+    return iconv.decode(response.body, charset[1].trim());
+  }
+
+  return response.body.toString('utf8');
+};
+
 module.exports = (url, options, parsers) => {
   if (!isURL(url)) {
     return Promise.reject(`URL is invalid: ${url}`);
@@ -18,34 +46,15 @@ module.exports = (url, options, parsers) => {
   parsers = parsers || {};
 
   return got(url, options).then(response => {
-    let result = jschardet.detect(response.body);
-    let body;
-    if (!result || !result.encoding || (result.confidence || 0) < 0.99) {
-      let head = response.body.toString('ascii').match(/<head[\s>]([\s\S]*?)<\/head>/i);
-      if (head) {
-        let charset = head[1].match(/<meta[^>]*[\s;]+charset\s*=\s*["']?([\w\-_]+)["']?/i);
-        if (charset) {
-          body = iconv.decode(response.body, charset[1].trim());
-        } else {
-          body = response.body.toString('utf8');
-        }
-      } else {
-        body = response.body.toString('utf8');
-      }
-    } else {
-      body = iconv.decode(response.body, result.encoding);
-    }
+    const body = getBody(response);
 
     if (!isHTML(body)) {
       return Promise.reject('Response is not HTML');
     }
 
-    let $ = cheerio.load(body);
-
+    const $ = cheerio.load(body);
     let data = {};
-    let keys = Object.keys(parsers);
-
-    for (let key of keys) {
+    for (let key of Object.keys(parsers)) {
       data[key] = parsers[key]($, url) || '';
     }
 
